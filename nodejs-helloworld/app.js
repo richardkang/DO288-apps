b18eee40c85ce976f4189cca7276657c3022342d
Student User <student@workstation.lab.example.com>
2020-02-17 15:19:36 +0000

    add msg

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 app = express();
-
+var msg = process.env.APP_MSG;
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! '+msg+' \n');
 });
 
 app.listen(8080, function () {
